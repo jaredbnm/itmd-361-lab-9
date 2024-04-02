8a18286189f44b2a7d75f2e682d083b007fd9a19
jaredbnm <jaredjr0204@gmail.com>
2024-04-02 13:44:35 -0500

    trying to fix button

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-function init() {
+function init(){
 //add your javascrip between these two lines of code
-    function showMeText() {
-      var textbox = document.getElementById('entryinput');
-      document.getElementById('textoutput').innerHTML = textbox.value;
-      alert("Jared Benman: " + textbox.value);
-    }
-    var button = document.getElementById('entrybutton');
-    button.addEventListener('click', showMeText)
-}
+  var button = document.getElementById('entrybutton');
+
+  function showMeText() {
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Jared Benman: " + textbox.value);
+  }
+  button.addEventListener('click', showMeText);
+  }
 window.addEventListener('load', init);
