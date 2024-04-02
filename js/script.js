2df279ba10c2704e9bd5c68f52f57d1ec36c0c41
jaredbnm <jaredjr0204@gmail.com>
2024-04-02 13:29:41 -0500

    added javascript code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 function init() {
 //add your javascrip between these two lines of code
-  var button = document.getElementById('entrybutton');
-  document.getElementById();
-
-
-
+    function showMeText() {
+      var textbox = document.getElementById('entryinput');
+      document.getElementById('textoutput').innerHTML = textbox.value;
+      alert("Jared Benman: " + textbox.value);
+    }
+    var button = document.getElementById('entrybutton');
+    button.addEventListener('click', showMeText)
 }
 window.addEventListener('load', init);
