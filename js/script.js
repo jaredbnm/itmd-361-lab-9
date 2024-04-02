0764fa17e4bed7a1cfb4acf8c2506a8f9c4709b0
jaredbnm <jaredjr0204@gmail.com>
2024-04-02 12:42:37 -0500

    adding javascript

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-function init(){
+function init() {
 //add your javascrip between these two lines of code
- 
+  var button = document.getElementById('entrybutton');
+  document.getElementById();
 
 
 
-
-
-
-
+}
 window.addEventListener('load', init);
