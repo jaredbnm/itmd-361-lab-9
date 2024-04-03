91b0d9d2ecf54b27e70597480ff50a8aa343b6f9
jaredbnm <jaredjr0204@gmail.com>
2024-04-02 19:24:44 -0500

    fixed to match final

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
   var button = document.getElementById('entrybutton');
-  button.addEventListener('click', output);
 
   function output() {
     var box = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = box.value;
     alert("Jared Benman: " + box.value);
   }
+  button.addEventListener('click', output);
 }
 window.addEventListener('load', init);
